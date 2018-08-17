f9ed92f6a2f3198dfa5562231c7a652dbf5916c1
hp\Administrator <912072089@qq.com>
2018-08-17 17:34:16 +0800

    *[koa-practice] 图片上传提交

--- vue-source-ala/koa-practice/util/upload.js
+++ vue-source-ala/koa-practice/util/upload.js
@@ -65,6 +65,9 @@ function uploadFile( ctx, options) {
       file.on('end', function() {
         result.success = true
         result.message = '文件上传成功'
+        result.data = {
+          pictureUrl: `//${ctx.host}/image/${fileType}/${fileName}`
+        }
         console.log('文件上传成功！')
       })
     })
