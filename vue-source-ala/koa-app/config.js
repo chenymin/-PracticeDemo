0308e2e1e5f7e190ac18e6e641b6aad98ed505a0
micky <chenyangmin@yuntujinfu.com>
2018-08-26 22:37:34 +0800

    *[koa-app] 练手代码

---
+++ vue-source-ala/koa-app/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+    port: 3001,
+
+    database: {
+        DATABASE: 'koa_demo',
+        USERNAME: 'root',
+        PASSWORD: 'cymmysql',
+        PORT: '3306',
+        HOST: 'localhost'
+    }
+}
+
+module.exports = config
---
+++ vue-source-ala/koa-app/server/controllers/admin.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+
+    async indexPage ( ctx ) {
+      const title = 'admin page'
+      await ctx.render('admin', {
+        title,
+      })
+    },
+  
+  }
