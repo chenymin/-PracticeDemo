f55c648d4fb2bdf4382f9c28f021f883bbe7e106
陈阳民 <chenym1@fosun.com>
2019-10-22 13:54:09 +0800

    *[practice-demo] proxy 代理source增加

--- my-custom-framework-master/src/index.js
+++ my-custom-framework-master/src/index.js
@@ -1,3 +1,4 @@
+// https://medium.com/@toastui/using-proxy-and-virtual-dom-to-build-your-own-framework-43ce9ddec81d
 import View from './js/view';
 
 const container = document.createElement('div');
--- my-custom-framework-master/webpack.config.js
+++ my-custom-framework-master/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
+    devtool: '#cheap-module-eval-source-map',
     plugins: [
         new HtmlWebpackPlugin()
     ]
